8878d963c9c9000942c59e0b7f7534673ea119e1
agung laksono <agunglaksono.it@gmail.com>
2019-03-21 13:29:29 +0700

    create sorting table

--- exercise3/swapi.js
+++ exercise3/swapi.js
@@ -1,16 +1,14 @@
 const url = 'https://swapi.co/api/planets/';
 
 
-
 function fetchDataSwapi(url) {
 
-    return fetch(url).then((respone) => respone.json());
+    return fetch(url).then((response) => response.json());
 
 }
 
 
-
-function tableRow(data) {
+function rowTable(data) {
 
     const row = document.createElement('tr');
 
@@ -29,33 +27,21 @@ function tableRow(data) {
 		surface_water, 
 		population,
 		residents,
-		films
-,
+		films,
 		created,
 		edited,
 		url
 	} = data;
 
-
-
     row.appendChild(element('td', name))
-
     row.appendChild(element('td',rotation_period))
-
     row.appendChild(element('td',orbital_period))
-
     row.appendChild(element('td', diameter))
-
     row.appendChild(element('td', climate))
-
     row.appendChild(element('td', gravity))
-
     row.appendChild(element('td', terrain))
-
     row.appendChild(element('td', surface_water))
-
     row.appendChild(element('td', population ))
-
     row.appendChild(element('td', residents,))
     row.appendChild(element('td', films))
     row.appendChild(element('td', created ))
@@ -63,16 +49,6 @@ function tableRow(data) {
     row.appendChild(element('td', url ))
 
 
-
-
-
-
-
-
-
-
-
-   
  return row;
 
 }
@@ -82,42 +58,31 @@ function tableRow(data) {
 function element(a,b,c) {
 
     const A = document.createElement(a);
-
     const content = document.createTextNode(b);
 
     A.appendChild(content);
 
-
     if (c) {
-
        A.classList.add(c);
-
     }
 
     return A;
 
 }
 
-
-
 const viewTablePlanets = document.getElementById('tablePlanets').getElementsByTagName('tbody')[0];
-
       
 fetchDataSwapi(url).then((data) => {
 
-
       data.results.forEach(result => {
-
-      const row = tableRow(result);
-
+      const row = rowTable(result);
       viewTablePlanets.appendChild(row);
-
    });
 
 });
 
 
-function myFunction() {
+function filteringTable() {
   var input, filter, table, tr, td, i;
   input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
@@ -132,5 +97,28 @@ function myFunction() {
         tr[i].style.display = "none";
       }
     }       
+  }
+}
+
+function sortTable() {
+  var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementById("tablePlanets");
+  switching = true;
+  while (switching) {
+    switching = false;
+    rows = table.rows;
+    for (i = 1; i < (rows.length - 1); i++) {
+      shouldSwitch = false;
+      x = rows[i].getElementsByTagName("TD")[0];
+      y = rows[i + 1].getElementsByTagName("TD")[0];
+      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
   }
 }
